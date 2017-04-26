d26bdda2eba174954e363f0d1d8adbabab591e76
carlnerv <spartan19910108@gmail.com>
2017-04-26 22:28:55 +0800

    继续增加代码
    照教材上的代码添加了colormaker.cpp中的代码

--- colormaker.cpp
+++ colormaker.cpp
@@ -1,6 +1,7 @@
 #include "colormaker.h"
 #include <QTimerEvent>
 #include <QDateTime>
+//#include <QTimer>
 
 ColorMaker::ColorMaker(QObject *parent)
     : QObject(parent)
@@ -19,4 +20,46 @@ ColorMaker::~ColorMaker()
 QColor ColorMaker::color() const
 {
     return m_currentColor;
+}
+
+void ColorMaker::setColor(const QColor &color)
+{
+    m_currentColor = color;
+    emit colorChanged(m_currentColor);
+}
+
+QColor ColorMaker::timeColor() const
+{
+    QTime time = QTime::currentTime();
+    int r = time.hour();
+    int g = time.minute()*2;
+    int b = time.second()*4;
+    return QColor::fromRgb(r,g,b);
+}
+
+ColorMaker::GenerateAlogrithm ColorMaker::algorithm() const
+{
+    return m_algorithm;
+}
+
+void ColorMaker::setAlgorithm(GenerateAlgorithm algorithm)
+{
+    m_algorithm = algorithm;
+}
+
+void ColorMaker::start()
+{
+    if(m_nColorTimer == 0)
+    {
+        m_nColorTimer = startTimer(1000);
+    }
+}
+
+void ColorMaker::stop()
+{
+    if(m_nColorTimer > 0)
+    {
+        killTimer(m_nColorTimer);
+        m_nColorTimer = 0;
+    }
 }
